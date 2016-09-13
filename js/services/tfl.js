7088d51856bf6981ae47993ea27610e0f24f02b4
rjcoll <robjcollier24@gmail.com>
2016-09-13 17:59:12 +0100

    Included station function

--- js/services/tfl.js
+++ js/services/tfl.js
@@ -2,16 +2,16 @@ angular.module('tflLiveData', []).
 service('Data', ['$http', '$q', function ($http, $q) {
   var my_Date = new Date();
   var rates = {};
-  var urlBase = function(id) {
-    return "https://api.tfl.gov.uk/StopPoint/" + id+ "/Arrivals?app_id=&app_key="
+  var urlBaseStop = function(key) {
+    return "https://api.tfl.gov.uk/StopPoint/" + key
   }
 
 
 
-  this.getTimes = function (id) {
+  this.getTimes = function (key) {
     return $http({
        method: 'Get',
-       url: urlBase(id),
+       url: urlBaseStop(key) + "/Arrivals?app_id=&app_key=",
     }).then(function(results) {
        console.log(results);
        return results.data
@@ -20,20 +20,57 @@ service('Data', ['$http', '$q', function ($http, $q) {
        return $q.reject(error);
     });
   };
+
+  this.getStation = function (key) {
+    return $http({
+       method: 'Get',
+       url: urlBaseStop(key),
+    }).then(function(results) {
+       return results.data
+    }, function(error) {
+       console.log(error);
+       return $q.reject(error);
+    });
+  };
 }]);
 
-app.controller('tflController', ['$scope', '$timeout', 'Data', function($scope, $timeout, Data) {
-  $scope.init = function(id) {
+app.controller('stationController', ['$scope','$http', function($scope, $http) {
+  $http.get('config.json')
+    .success(function(data) {
+      $scope.list = data.list
+    });
+}]);
+
+
+
+
+  app.controller('tflDataController', ['$scope', '$timeout', 'Data', function($scope, $timeout, Data) {
+    var key = $scope.stop
     $scope.data = [];
-    var rates = {};
+    $scope.station = "";
+
+    Data.getStation(key).then(function(data){
+      $scope.station = data
+      console.log(data)
+    });
+
     (function tick() {
-      Data.getTimes(id).then(function(data){
+      Data.getTimes(key).then(function(data){
         $scope.data = data;
-        console.log(data)
+        console.log(data);
       })['finally'](function() {
         $timeout(tick, 1000);
       });
     })();
-  }
-
 }]);
+
+app.directive('tablewidget', function(){
+    return {
+      restrict: "E",
+      scope: {
+        stop: "="
+      },
+      templateUrl : 'js/services/tableWidget.html',
+      controller: 'tflDataController'
+    }
+  })
